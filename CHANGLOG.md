47029ab16b0a8a2abdd2b0199c0829b3a03d5e73
jiangqi.rrt <jiangqi.rrt@bytedance.com>
2025-09-25 09:57:11 +0800

    version

--- CHANGLOG.md
+++ CHANGLOG.md
@@ -1,3 +1,6 @@
+## [0.1.13] - 2025-09-24
+### Added
+- support custom trace connect ptaas trace
 
 ## [0.1.0] - 2025-04-07
 
--- internal/version.go
+++ internal/version.go
@@ -5,5 +5,5 @@ package internal
 
 // Version returns the version of the loop package.
 func Version() string {
-	return "v0.1.12"
+	return "v0.1.13"
 }
